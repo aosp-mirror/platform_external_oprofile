245b75880b4a7ad329b53ab9ee8a12d4fafee368
Joe Onorato <joeo@google.com>
2011-04-07 18:40:59 -0700

    This doesn't always build, so completely disable it.
    Change-Id: Ie87d9459a98fbd45619bf1da83c2e68fb63b075a

--- Android.mk
+++ Android.mk
@@ -12,6 +12,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-ifeq ($(TARGET_SIMULATOR),false)
-    include $(call all-subdir-makefiles)
-endif
+#ifeq ($(TARGET_SIMULATOR),false)
+#    include $(call all-subdir-makefiles)
+#endif
