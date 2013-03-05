935b17dd32d856eff6cdacecaee0b66cd00af69d
Paul Lind <paul.lind@imgtec.com>
2013-03-04 17:23:51 -0800

    Add oprofile host tool support for mips.
    Provides general build support, and fixes one arm-specific change
    in opannotate.
    
    Change-Id: Ib4f8145719fb6b2a1e292b72c2bed044497c55c2

--- common.mk
+++ common.mk
@@ -36,6 +36,15 @@ HAVE_LIBBFD := false
 
 ifeq ($(TARGET_ARCH),arm)
 toolchain := prebuilts/gcc/$(HOST_PREBUILT_TAG)/arm/arm-linux-androideabi-4.6
+common_host_extra_flags := -DANDROID_TARGET_ARM
+endif
+
+ifeq ($(TARGET_ARCH),mips)
+toolchain := prebuilts/gcc/$(HOST_PREBUILT_TAG)/mips/mipsel-linux-android-4.6
+common_host_extra_flags := -DANDROID_TARGET_MIPS
+endif
+
+ifneq ($(filter arm mips,$(TARGET_ARCH)),)
 common_host_c_includes := $(common_c_includes) $(toolchain)/include
 common_host_cflags := $(common_cflags) -fexceptions -DANDROID_HOST -DHAVE_XCALLOC
 common_host_ldlibs_libiconv :=
